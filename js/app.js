f8d61b2a71c12fa24ee850721468fd2a72055c8e
yoonjungy <jchoi.yoon@gmail.com>
2016-11-05 21:41:06 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var showImage = function(e)
+{
+	var imageArray = document.getElementsByClassName('showableImage hidden');
+	var image = imageArray[0];
+	image.className = "showableImage";
+
+    var xPosition = e.clientX;
+    var yPosition = e.clientY;
+	console.log(xPosition + ", " + yPosition);
+	
+	var centerX = xPosition - (Number(image.clientWidth)/2);
+	var centerY = yPosition - (Number(image.clientHeight)/2);
+	console.log(centerX + ", " + centerY);
+	image.style.left = centerX +"px";
+	image.style.top = centerY +"px";
+}
+
+document.onclick = showImage;
